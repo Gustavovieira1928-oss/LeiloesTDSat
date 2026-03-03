3c06a2e2c051694fd42f9ae99c5d8928a7f12bd2
Gustavovieira1928-oss <gustavovieira1928@gmail.com>
2026-03-03 15:46:50 -0300

    Commit #2: listagem de produtos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -39,10 +39,29 @@ public class ProdutosDAO {
     }
 }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
+  public ArrayList<ProdutosDTO> listarProdutos(){
+    String sql = "SELECT * FROM produtos";
+    conn = new conectaDAO().connectDB();
+    
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
         
-        return listagem;
+        listagem.clear(); // Limpa a lista para não duplicar na tela
+        
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            
+            listagem.add(produto);
+        }
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar: " + e.getMessage());
     }
-         
+    return listagem;
+}    
 }
 
