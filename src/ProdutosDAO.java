8ba44d1f4e5fdc9a2ef9b5c9c5fccd8898e4cebe
Gustavovieira1928-oss <gustavovieira1928@gmail.com>
2026-03-05 18:13:10 -0300

    Commit #1: Implementados métodos venderProduto e listarProdutosVendidos no DAO

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -63,5 +63,41 @@ public class ProdutosDAO {
     }
     return listagem;
 }    
+  
+  public void venderProduto(int id) {
+    conn = new conectaDAO().connectDB();
+    String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+    
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setInt(1, id);
+        prep.executeUpdate();
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao vender: " + e.getMessage());
+    }
+}
+  
+  public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+    String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+    conn = new conectaDAO().connectDB();
+    
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+        listagem.clear(); 
+        
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            listagem.add(produto);
+        }
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar vendidos: " + e.getMessage());
+    }
+    return listagem;
+}
 }
 
